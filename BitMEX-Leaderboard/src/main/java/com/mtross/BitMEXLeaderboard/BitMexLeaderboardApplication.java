118f6e71401f70e53519d82984db06f20c2e8017
Michael <michaeltross0@gmail.com>
2019-12-05 16:34:22 -0500

    Added a file made with the online tool Spring Initializr, and made the applications.properties file.

---
+++ BitMEX-Leaderboard/src/main/java/com/mtross/BitMEXLeaderboard/BitMexLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.mtross.BitMEXLeaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitMexLeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitMexLeaderboardApplication.class, args);
+	}
+
+}
