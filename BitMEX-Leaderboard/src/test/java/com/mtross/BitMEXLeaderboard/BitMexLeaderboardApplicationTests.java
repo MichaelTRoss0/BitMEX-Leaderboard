f20fce803330a37390c0ad22ae6cc62c8b6b7150
Michael <michaeltross0@gmail.com>
2019-12-05 16:34:22 -0500

    Added a file made with the online tool Spring Initializr, and made the applications.properties file.

---
+++ BitMEX-Leaderboard/src/test/java/com/mtross/BitMEXLeaderboard/BitMexLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtross.BitMEXLeaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitMexLeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
