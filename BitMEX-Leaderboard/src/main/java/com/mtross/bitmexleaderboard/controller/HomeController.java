bc002be7d671164c03c931567edebac6b2369b62
Michael <michaeltross0@gmail.com>
2019-12-13 17:37:04 -0500

    Trying to create a framework to meet project specifications.

--- BitMEX-Leaderboard/src/main/java/com/mtross/bitmexleaderboard/controller/HomeController.java
+++ BitMEX-Leaderboard/src/main/java/com/mtross/bitmexleaderboard/controller/HomeController.java
@@ -5,8 +5,11 @@
  */
 package com.mtross.bitmexleaderboard.controller;
 
-import com.mtross.bitmexleaderboard.dao.LeaderboardRepository;
-import com.mtross.bitmexleaderboard.dao.UserRepository;
+import com.mtross.bitmexleaderboard.entity.Leaderboard;
+import com.mtross.bitmexleaderboard.service.LeaderboardService;
+import java.time.Clock;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,18 +23,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @Autowired
-    UserRepository user;
-
-    @Autowired
-    LeaderboardRepository leaderboard;
+    LeaderboardService service;
+    
+    private final Clock utcClock = Clock.systemUTC();
+    private final ZoneOffset utcOffset = ZoneOffset.UTC;
+    private final ZoneId utc = ZoneId.of("Z");
 
     @GetMapping("")
     public String getIndex(Model model) {
-        return displayChangesInProfit(model);
+        return getLeaderboards(model);
     }
 
     @GetMapping("/home")
-    public String displayChangesInProfit(Model model) {
+    public String getLeaderboards(Model model) {
+        Leaderboard leaderboardTDY = new Leaderboard();
+        Leaderboard leaderboardYDA = new Leaderboard();
+        
+        
+        
+        model.addAttribute("leaderboardTDY", leaderboardTDY);
+        model.addAttribute("leaderboardYDA", leaderboardYDA);
 
         return "home";
     }
